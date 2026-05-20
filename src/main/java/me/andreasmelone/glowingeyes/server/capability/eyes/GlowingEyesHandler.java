cdcb59fb9f7a21232953746cb8e33a63a92add09
fracic2005-jpg <fracic2005@gmail.com>
2026-05-20 12:08:57 -0700

    Update GlowingEyesHandler.java

--- src/main/java/me/andreasmelone/glowingeyes/server/capability/eyes/GlowingEyesHandler.java
+++ src/main/java/me/andreasmelone/glowingeyes/server/capability/eyes/GlowingEyesHandler.java
@@ -7,11 +7,14 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.scores.Objective;
+import net.minecraft.world.scores.Scoreboard;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.common.util.INBTSerializable;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -38,7 +41,6 @@ public class GlowingEyesHandler implements INBTSerializable<CompoundTag>, ICapab
 
     @Override
     public @NotNull <T> LazyOptional<T> getCapability(@NotNull Capability<T> capability, @Nullable Direction direction) {
-        // This explicitly links the handler to the Capability constant defined in GlowingEyesCapability
         if (capability == GlowingEyesCapability.INSTANCE) {
             return instance.cast();
         }
@@ -48,8 +50,28 @@ public class GlowingEyesHandler implements INBTSerializable<CompoundTag>, ICapab
     @SubscribeEvent
     public static void attach(final AttachCapabilitiesEvent<Entity> event) {
         if(event.getObject() instanceof Player) {
-            GlowingEyesHandler glowingEyesHandler = new GlowingEyesHandler();
-            event.addCapability(GlowingEyesHandler.IDENTIFIER, glowingEyesHandler);
+            event.addCapability(GlowingEyesHandler.IDENTIFIER, new GlowingEyesHandler());
+        }
+    }
+
+    @SubscribeEvent
+    public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
+        if (event.phase == TickEvent.Phase.END && !event.player.level().isClientSide) {
+            Player player = event.player;
+            Scoreboard scoreboard = player.getScoreboard();
+            Objective objective = scoreboard.getObjective("gloweyes");
+
+            if (objective != null && scoreboard.hasPlayerScore(player.getScoreboardName(), objective)) {
+                int score = scoreboard.getOrCreatePlayerScore(player.getScoreboardName(), objective).getScore();
+                boolean shouldBeOn = (score == 1);
+                
+                player.getCapability(GlowingEyesCapability.INSTANCE).ifPresent(cap -> {
+                    if (cap.isToggledOn() != shouldBeOn) {
+                        cap.setToggledOn(shouldBeOn);
+                        GlowingEyesCapability.sendUpdate(player);
+                    }
+                });
+            }
         }
     }
 }
